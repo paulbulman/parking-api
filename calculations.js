728a81e343e60af1f5943bd329356a635b2f2493
Paul Bulman <paulbulman@gmail.com>
2019-12-31 18:19:07 +0000

    Refactor

--- calculations.js
+++ calculations.js
@@ -1,19 +1,21 @@
 const moment = require("moment-timezone");
 
+const timeZoneName = "Europe/London";
+const dateFormatString = "YYYY-MM-DD";
+
+const getCurrentDate = getMoment =>
+  getMoment()
+    .clone()
+    .tz(timeZoneName)
+    .startOf("date");
+
 const isWorkingDay = (date, bankHolidays) =>
   date.isoWeekday() !== 6 &&
   date.isoWeekday() !== 7 &&
   bankHolidays.every(b => !date.isSame(b, "day"));
 
-const getCurrentDate = getMoment =>
-  getMoment()
-    .clone()
-    .tz("Europe/London")
-    .startOf("date");
-
 const getCurrentActiveDates = (getMoment, bankHolidays) => {
   const currentDate = getCurrentDate(getMoment);
-
   const lastDayOfNextMonth = currentDate
     .clone()
     .startOf("month")
@@ -21,19 +23,16 @@ const getCurrentActiveDates = (getMoment, bankHolidays) => {
     .subtract(1, "days");
 
   let activeDates = [];
-  for (
-    let d = currentDate.clone();
-    d <= lastDayOfNextMonth;
-    d = d.add(1, "days")
-  ) {
-    activeDates.push(d.clone());
+  while (currentDate.isSameOrBefore(lastDayOfNextMonth, "day")) {
+    activeDates.push(currentDate.clone());
+    currentDate.add(1, "days");
   }
 
   return activeDates.filter(d => isWorkingDay(d, bankHolidays));
 };
 
 module.exports = (getMoment, bankHolidays) => {
-  const toDateString = date => date.format("YYYY-MM-DD");
+  const toDateString = date => date.format(dateFormatString);
 
   return {
     getCurrentDateString: () => toDateString(getCurrentDate(getMoment)),
